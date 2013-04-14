c365264216c7758fd45f256c06b99403cda9f433
Adam <adam.see80@gmail.com>
2013-04-14 14:56:58 -0600

    This commit adds very basic functionality to the app.  It adds the Map screen with the needed additions to the manifest, and other files required in order to make the map screen work (google play lib, map jars, etc.)

--- src/com/android/GPS_Caddy/activity/HomeScreen.java
+++ src/com/android/GPS_Caddy/activity/HomeScreen.java
@@ -2,7 +2,11 @@ package com.android.GPS_Caddy.activity;
 
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import com.android.GPS_Caddy.R;
 import org.holoeverywhere.widget.Button;
@@ -15,11 +19,13 @@ import org.holoeverywhere.widget.Toast;
  * Time: 10:22 PM
  * To change this template use File | Settings | File Templates.
  */
-public class HomeScreen extends Activity {
+public class HomeScreen extends FragmentActivity implements View.OnClickListener {
     private Button clubsButton;
     private Button courseMapButton;
     private Button courseButton;
     private Button videoButton;
+    private Typeface tf;
+    private int test = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,19 +35,31 @@ public class HomeScreen extends Activity {
     }
 
     private void initControls() {
+        tf = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Regular.ttf");
         clubsButton = (Button) findViewById(R.id.clubsButton);
+        clubsButton.setTypeface(tf);
         courseMapButton = (Button) findViewById(R.id.mapsButton);
+        courseMapButton.setTypeface(tf);
         courseButton = (Button) findViewById(R.id.courseButton);
+        courseButton.setTypeface(tf);
         videoButton = (Button) findViewById(R.id.videoButton);
+        videoButton.setTypeface(tf);
+
+        clubsButton.setOnClickListener(this);
+        courseButton.setOnClickListener(this);
+        courseMapButton.setOnClickListener(this);
+        videoButton.setOnClickListener(this);
     }
 
-    public void onBtnClick(View v) {
+    @Override
+    public void onClick(View v) {
         switch(v.getId()) {
             case R.id.clubsButton:
                 Toast.makeText(this, "Clubs button pressed", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.mapsButton:
-
+                Intent intent = new Intent(this, MapScreen.class);
+                startActivity(intent);
                 break;
             case R.id.courseButton:
 
